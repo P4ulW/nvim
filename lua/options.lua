54cd24d789dc027c92a8dff40f7af2ae1b926215
Paul Werner <paul.werner@stud.uni-goettingen.de>
2024-09-19 17:08:33 +0200

    added slime

--- lua/options.lua
+++ lua/options.lua
@@ -3,6 +3,14 @@ require("nvim-tree").setup({
     view={side="right"}
   })
 
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
 -- add yours here!
 
 -- local o = vim.o
