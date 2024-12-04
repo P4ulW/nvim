db4296dfb5f3c26ee5b42f11823e7d9529bc3ecc
Paul Werner <paul.werner@stud.uni-goettingen.de>
2024-12-04 17:31:55 +0100

    catpp

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -4,7 +4,7 @@
 ---@type ChadrcConfig
 local M = {}
 M.base46 = {
-	theme = "rosepine",
+	theme = "catppuccin",
   transparency=true,
 	hl_override = {
     CurSearch = {fg='#1e1d2d', bg='#ff4558'},
