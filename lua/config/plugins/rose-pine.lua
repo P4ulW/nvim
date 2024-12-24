6f3ffc045fc3fc5a92a86891c44641236223f79b
Paul Werner <paul.werner@studi.uni-goettingen.de>
2024-12-24 01:06:36 +0100

    edited rosepine to have less italic

--- lua/config/plugins/rose-pine.lua
+++ lua/config/plugins/rose-pine.lua
@@ -3,7 +3,18 @@ return {
   name = "rose-pine",
   config = function()
     require("rose-pine").setup({
-      variant = "dark"
+      variant = "dark",
+      styles = {
+        italic = true,
+        transparency = true,
+      },
+      highlight_groups = {
+        ["@variable"] = { fg = "text", italic = false },
+        ["@variable.parameter"] = { fg = "iris", italic = true },
+        ["@property"] = { fg = "foam", italic = false },
+        ["@variable.builtin"] = { fg = "love", bold = false, italic = true },
+      },
+
     })
     vim.cmd("colorscheme rose-pine")
   end
