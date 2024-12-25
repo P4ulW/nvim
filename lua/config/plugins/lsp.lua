281c8d774fd1470ff9438b9775e01ebbc8992630
Paul Werner <paul.werner@stud.uni-goettingen.de>
2024-12-25 19:44:07 +0100

    added options for rst and c files, added lsp renamer

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,5 +1,38 @@
 local map = vim.keymap.set
 
+local renamer = function()
+  local api = vim.api
+  local var = vim.fn.expand "<cword>"
+  local buf = api.nvim_create_buf(false, true)
+  local opts = { height = 1, style = "minimal", border = "rounded", row = 1, col = 1 }
+
+  opts.relative, opts.width = "cursor", #var + 15
+  opts.title, opts.title_pos = { { " Rename ", "@comment.danger" } }, "center"
+
+  local win = api.nvim_open_win(buf, true, opts)
+  vim.wo[win].winhl = "Normal:Normal,FloatBorder:Removed"
+  api.nvim_set_current_win(win)
+
+  api.nvim_buf_set_lines(buf, 0, -1, true, { " " .. var })
+
+  vim.bo[buf].buftype = "prompt"
+  vim.fn.prompt_setprompt(buf, "")
+  vim.api.nvim_input "A"
+
+  vim.keymap.set({ "i", "n" }, "<Esc>", "<cmd>q<CR>", { buffer = buf })
+
+  vim.fn.prompt_setcallback(buf, function(text)
+    local newName = vim.trim(text)
+    api.nvim_buf_delete(buf, { force = true })
+
+    if #newName > 0 and newName ~= var then
+      local params = vim.lsp.util.make_position_params()
+      params.newName = newName
+      vim.lsp.buf_request(0, "textDocument/rename", params)
+    end
+  end)
+end
+
 local M = {}
 
 -- taken from nvchad
@@ -14,6 +47,7 @@ M.on_attach = function(_, bufnr)
   map("n", "<space>sh", vim.lsp.buf.signature_help, opts "Show signature help")
   map("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts "Add workspace folder")
   map("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts "Remove workspace folder")
+  map("n", "<space>ra", renamer, opts "Rename")
 
   map("n", "<space>wl", function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
@@ -60,6 +94,20 @@ return {
         pyright = {
           filetype = { "python" }
         },
+        clangd = {
+          filetype = { "c" }
+        },
+        bacon_ls = {
+          settings = {
+
+            filetype = { "rust", "rs" },
+            init_options = {
+              spawnBacon = true,
+              spawnBaconCommand = "bacon clippy -- --all-features",
+              updateOnSave = true,
+            },
+          }
+        }
       }
     },
 
