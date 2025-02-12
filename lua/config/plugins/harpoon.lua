c4b7fa43627bebdf3bac063a675bfdf8bd2cd63c
Paul Werner <paul.werner@stud.uni-goettingen.de>
2025-02-12 17:54:38 +0100

    snippets and harpoon conf

--- lua/config/plugins/harpoon.lua
+++ lua/config/plugins/harpoon.lua
@@ -9,7 +9,7 @@ return {
     map("n", "<space>ha", function() harpoon:list():add() end, { desc = "harpoon current file" })
     map("n", "<space>hd", function() harpoon:list():clear() end, { desc = "harpoon clear list" })
     map("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "harpoon toogle quick" })
-
+    map("n", "<space>hl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "harpoon toogle quick" })
 
     map("n", "<C-1>", function() harpoon:list():select(1) end)
     map("n", "<C-2>", function() harpoon:list():select(2) end)
