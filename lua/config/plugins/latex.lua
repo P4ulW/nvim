3fb0c0568396b1f0816049f8ed020f43de37835f
Paul Werner <paul.werner@stud.uni-goettingen.de>
2025-04-22 13:59:36 +0200

    added  space ll for vimtex

--- lua/config/plugins/latex.lua
+++ lua/config/plugins/latex.lua
@@ -1,7 +1,12 @@
+local map = vim.keymap.set
+
 return {
   "lervag/vimtex",
   lazy = false, -- we don't want to lazy load VimTeX
   config = function()
     vim.g.vimtex_view_general_viewer = 'okular'
+    -- vim.g.vimtex_view_general_options = '--unique file:@pdf/#src:@line@tex'
+    vim.g.tex_flavor = 'latex'
+    map('n', "<space>ll", "<cmd>VimtexCompile<CR>")
   end,
 }
