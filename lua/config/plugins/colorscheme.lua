7da33502f835fb07c3c84b191bc44ec8257dbdd0
Paul Werner <paul.werner@stud.uni-goettingen.de>
2025-05-27 13:57:31 +0200

    color: switched to rosepine

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -1,49 +1,49 @@
-return {
-  "rebelot/kanagawa.nvim",
-  name = "kanagawa",
-  config = function()
-    require('kanagawa').setup({
-      compile = false,  -- enable compiling the colorscheme
-      undercurl = true, -- enable undercurls
-      commentStyle = { italic = true },
-      functionStyle = {},
-      keywordStyle = { italic = true },
-      statementStyle = { bold = true },
-      typeStyle = {},
-      transparent = false,   -- do not set background color
-      dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
-      terminalColors = true, -- define vim.g.terminal_color_{0,17}
-      theme = "dragon",      -- Load "wave" theme
-      background = {         -- map the value of 'background' option to a theme
-        dark = "dragon",     -- try "dragon" !
-        light = "lotus"
-      },
-    })
-    vim.cmd("colorscheme kanagawa")
-  end
-}
-
-
 -- return {
---   "rose-pine/neovim",
---   name = "rose-pine",
+--   "rebelot/kanagawa.nvim",
+--   name = "kanagawa",
 --   config = function()
---     require("rose-pine").setup({
---       variant = "moon",
---       styles = {
---         italic = true,
---         transparency = true,
---       },
---       highlight_groups = {
---         ["@variable"] = { fg = "text", italic = false },
---         ["@variable.parameter"] = { fg = "iris", italic = true },
---         ["@property"] = { fg = "foam", italic = false },
---         ["@variable.builtin"] = { fg = "love", bold = false, italic = true },
+--     require('kanagawa').setup({
+--       compile = false,  -- enable compiling the colorscheme
+--       undercurl = true, -- enable undercurls
+--       commentStyle = { italic = true },
+--       functionStyle = {},
+--       keywordStyle = { italic = true },
+--       statementStyle = { bold = true },
+--       typeStyle = {},
+--       transparent = false,   -- do not set background color
+--       dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
+--       terminalColors = true, -- define vim.g.terminal_color_{0,17}
+--       theme = "dragon",      -- Load "wave" theme
+--       background = {         -- map the value of 'background' option to a theme
+--         dark = "dragon",     -- try "dragon" !
+--         light = "lotus"
 --       },
 --     })
---     vim.cmd("colorscheme rose-pine")
+--     vim.cmd("colorscheme kanagawa")
 --   end
 -- }
+
+
+return {
+  "rose-pine/neovim",
+  name = "rose-pine",
+  config = function()
+    require("rose-pine").setup({
+      variant = "moon",
+      styles = {
+        italic = true,
+        transparency = true,
+      },
+      highlight_groups = {
+        ["@variable"] = { fg = "text", italic = false },
+        ["@variable.parameter"] = { fg = "iris", italic = true },
+        ["@property"] = { fg = "foam", italic = false },
+        ["@variable.builtin"] = { fg = "love", bold = false, italic = true },
+      },
+    })
+    vim.cmd("colorscheme rose-pine")
+  end
+}
 
 -- return {
 --   "catppuccin/nvim",
