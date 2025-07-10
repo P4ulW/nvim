6d866e159c0d79bd0394a130e38cee7eb373f73a
Paul Werner <paul.werner@stud.uni-goettingen.de>
2025-07-10 13:03:02 +0200

    removed after, added stuff from nvim-lite

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,8 +1,53 @@
-local set = vim.opt
-local map = vim.keymap.set
-set.clipboard = "unnamedplus"
-set.grepprg = "rg --vimgrep"
-set.grepformat = "%f:%l:%c:%m"
+local set            = vim.opt
+local map            = vim.keymap.set
+set.clipboard        = "unnamedplus"
+set.grepprg          = "rg --vimgrep"
+set.grepformat       = "%f:%l:%c:%m"
+
+-- Basic settings
+set.number           = true
+set.relativenumber   = true
+set.scrolloff        = 10
+set.breakindent      = true
+set.showbreak        = '...'
+set.signcolumn       = 'yes:1'
+set.colorcolumn      = '80'
+
+-- indentation
+set.shiftwidth       = 4
+set.tabstop          = 4
+set.expandtab        = true
+set.autoindent       = true
+
+-- spelling
+set.spell            = true
+set.spelllang        = "en"
+--
+-- Search settings
+set.ignorecase       = true -- Case insensitive search
+set.smartcase        = true -- Case sensitive if uppercase in search
+set.hlsearch         = true -- Don't highlight search results
+set.incsearch        = true -- Show matches as you type
+
+-- File handling
+set.backup           = false                           -- Don't create backup files
+set.writebackup      = false                           -- Don't create backup before writing
+set.swapfile         = false                           -- Don't create swap files
+set.undofile         = true                            -- Persistent undo
+set.undodir          = vim.fn.expand("~/.vim/undodir") -- Undo directory
+set.updatetime       = 300                             -- Faster completion
+set.timeoutlen       = 500                             -- Key timeout duration
+set.ttimeoutlen      = 0                               -- Key code timeout
+set.autoread         = true                            -- Auto reload files changed outside vim
+set.autowrite        = false                           -- Don't auto save
+
+-- Split behavior
+set.splitbelow       = true -- Horizontal splits go below
+set.splitright       = true -- Vertical splits go right
+
+-- Key mappings
+vim.g.mapleader      = " " -- Set leader key to space
+vim.g.maplocalleader = " " -- Set local leader key (NEW)
 
 
 require("nvim-tree").setup({
@@ -60,18 +105,24 @@ map("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", { desc = "nvimtree focus window"
 -- floaterminal
 map({ 'n', 't' }, "<A-i>", "<cmd>Floaterminal<CR>", { desc = "toggle floaterminal" })
 
-map({ "n" }, "<C-u>", "<C-u>zz")
-map({ "n" }, "<C-d>", "<C-d>zz")
 
-set.scrolloff      = 10
-set.spell          = true
-set.spelllang      = "en"
-set.relativenumber = true
-set.breakindent    = true
-set.showbreak      = '...'
-set.signcolumn     = 'yes:1'
-set.colorcolumn    = '80'
+-- Center screen when jumping
+map("n", "n", "nzzzv", { desc = "Next search result (centered)" })
+map("n", "N", "Nzzzv", { desc = "Previous search result (centered)" })
+map("n", "<C-d>", "<C-d>zz", { desc = "Half page down (centered)" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Half page up (centered)" })
 
+-- Better indenting in visual mode
+map("v", "<", "<gv", { desc = "Indent left and reselect" })
+map("v", ">", ">gv", { desc = "Indent right and reselect" })
+
+-- Buffer navigation
+map("n", "<leader>bn", ":bnext<CR>", { desc = "Next buffer" })
+map("n", "<leader>bp", ":bprevious<CR>", { desc = "Previous buffer" })
+
+-- Tab display settings
+set.showtabline = 1 -- Always show tabline (0=never, 1=when multiple tabs, 2=always)
+set.tabline = ''    -- Use default tabline (empty string uses built-in)
 
 -- set pwsh as windows terminal
 if vim.g.neovide then
