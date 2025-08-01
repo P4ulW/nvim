535baeab932ae3fa22f1b46dfc43cfce7d0e8c4e
Paul Werner <paul.werner@stud.uni-goettingen.de>
2025-08-01 13:26:42 +0200

    changed latex doc viewer and slime to tmux

--- lua/config/plugins/latex.lua
+++ lua/config/plugins/latex.lua
@@ -4,7 +4,7 @@ return {
   "lervag/vimtex",
   lazy = false, -- we don't want to lazy load VimTeX
   config = function()
-    vim.g.vimtex_view_general_viewer = 'okular'
+    vim.g.vimtex_view_general_viewer = 'evince'
     -- vim.g.vimtex_view_general_options = '--unique file:@pdf/#src:@line@tex'
     vim.g.tex_flavor = 'latex'
     map('n', "<space>ll", "<cmd>VimtexCompile<CR>")
